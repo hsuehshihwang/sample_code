657e94875e711794ab8ff32603b95ee0c67c9997
Ralph Wang <ralph@status.com.tw>
2023-03-22 16:24:36 +0800

    update sample code

--- update.sh
+++ update.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 git add .
-# git commit . -m  $( [[ $1 = "" ]] && echo -ne "update sample code" || echo -ne $1 )
-git commit . -m "update"
+git commit -m  "$( [[ $1 = "" ]] && echo -ne "update sample code" || echo -ne "$1" )"
 git push origin master 
